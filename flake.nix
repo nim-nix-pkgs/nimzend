274b168f7412e4beccee8200d9a70ddc2f071432
hugosenari <hugosenari@gmail.com>
2022-03-12 04:33:18 -0300

    chore: re index nimzend

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Native Nim Zend API glue for easy PHP extension development.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimzend-master".url = "path:./master";
+  inputs."nimzend-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimzend-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
