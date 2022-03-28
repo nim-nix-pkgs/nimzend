e26490266237d58c2c726971ad2f1e193e5b0089
hugosenari <hugosenari@gmail.com>
2022-03-27 21:53:52 -0300

    chore: re index nimzend

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-nimzend-master.ref   = "refs/heads/master";
   inputs.src-nimzend-master.owner = "metatexx";
   inputs.src-nimzend-master.repo  = "nimzend";
-  inputs.src-nimzend-master.dir   = "";
   inputs.src-nimzend-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
